0592c4334f771f0d705f04647c179e1f23599d59
yedeuwu <tim.benzaquinphilippot@gmail.com>
2026-04-24 16:58:19 +0200

    Modified files names for clarity

--- scripts/midi_manager.py
+++ scripts/midi_manager.py
@@ -1,4 +1,4 @@
-import mido
+import mido as md
 
 
 class MidiManager:
@@ -10,3 +10,10 @@ class MidiManager:
     @classmethod
     def verify(cls, filepath: str) -> tuple[list[str]]:
         pass
+    
+    
+f1 = "../midi tests/148BPM chords.midi"
+f2 = "../midi tests/148BPM mel.midi"
+mid1 = md.MidiFile(f1)
+mid2 = md.MidiFile(f2)
+print(mid1,mid2)
