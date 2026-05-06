7249f138621c8f1e7eea4d8e5f5f50da83d69375
Clément MARTY <clement_marty@outlook.fr>
2026-05-06 21:18:05 +0200

    Add method docstrings

--- app/channel_assignment.py
+++ app/channel_assignment.py
@@ -1,29 +1,53 @@
 import tkinter as tk
 
 
-class ChannelAssignmentWindow(tk.Toplevel):
-    
-    def __init__(self, master: tk.Tk) -> None:
-        pass
-
-    def create_widgets(self) -> None:
-        pass
-
-    def update_assignments(self, coil: int, channel: int, value: bool) -> None:
-        pass
-
-    def close(self, event: tk.Event) -> None:
-        pass
-
-
-
 class ChannelAssignmentRow(tk.Frame):
 
-    def __init__(self, master: tk.Tk) -> None:
+    def __init__(self, master: tk.Tk, coil_id: int, coil_name: str) -> None:
+        '''Tkinter widget representing a file
+
+        :param tkinter.Tk master: The widget's parent
+        :param int coil_id: The tesla coil's id
+        :param str coil_name: The tesla coil's name
+        '''
         pass
 
     def create_widgets(self) -> None:
+        '''Creates and places all the window's widgets
+        '''
         pass
 
     def toggle_btn_state(self, event: tk.Event):
+        '''Toggles a button on/off and updates the associated ChannelAssignmentWindow object
+        
+        :param tkinter.Event event: The event that triggered the function's execution
+        '''
+        pass
+
+
+
+class ChannelAssignmentWindow(tk.Toplevel):
+    
+    def __init__(self, master: tk.Tk) -> None:
+        '''A Tkinter window handling the assignement of MIDI channels on tesla coils
+        '''
+        pass
+
+    def create_widgets(self) -> None:
+        '''Creates and places all the window's widgets
+        '''
+        pass
+
+    def update_assignments(self, coil: int, channel: int, value: bool) -> None:
+        '''Updates the assignments dictionary for a given coil and MIDI channel
+        
+        :param int coil: The tesla coil's id
+        :param int channel: The MIDI channel's id
+        :param bool value: The new value to be set
+        '''
+        pass
+
+    def close(self, event: tk.Event) -> None:
+        '''Closes the window
+        '''
         pass
