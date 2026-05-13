7219e91ee42111b37ae273aa6cc528f76075951d
arevkova <revkovaanastasiia@gmail.com>
2026-05-13 14:13:11 +0200

    starting window - first try - window and several basic widgets

--- app/start.py
+++ app/start.py
@@ -7,16 +7,34 @@ from .configuration import ConfigurationWindow
 class StartWindow(tk.Tk):
 
     def __init__(self) -> None:
-        pass
+        super().__init__()
+        self.title("MIDI TOOLS")
+        self.geometry("400x200")
+        self.create_widgets()
 
-    def create_widgets(self) -> None:
-        pass
+    def create_widgets(self):
+        lbl_1 = tk.Label(self, text = "Configuration")
+        lbl_1.grid(row =1, column = 1)
+        lbl_1.button.bind("<Button-1>", self.open_configuration_window)
 
-    def open_merge_window(self, event: tk.Event) -> None:
-        pass
+        lbl_2 = tk.Label(self, text = "Verification")
+        lbl_2.grid(row =2, column = 1)
+        lbl_2.button.bind("<Button-1>", self.open_verification_window)
 
-    def open_verification_window(self, event: tk.Event) -> None:
-        pass
+        lbl_3 = tk.Label(self, text = "Merge")
+        lbl_3.grid(row =3, column = 1)
+        lbl_3.button.bind("<Button-1>", self.open_merge_window)
+
+    def open_merge_window(self, event: tk.Event):
+        merge_window = tk.Toplevel()  # creates a second window
+        merge_window.title("Merge Window")
+
+    def open_verification_window(self, event: tk.Event):
+        verification_window = tk.Toplevel()  # creates a second window
+        verification_window.title("Verification Window")
 
     def open_configuration_window(self, event: tk.Event) -> None:
-        pass
+        configuration_window = tk.Toplevel()  # creates a second window
+        configuration_window.title("Configuration Window")
+        
+
