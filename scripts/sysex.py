e264b8f4c94c49cf5e21ce92add89acd21b47892
Clément MARTY <clement_marty@outlook.fr>
2026-05-22 16:00:18 +0200

    Add SysExConfiguration constructor

--- scripts/sysex.py
+++ scripts/sysex.py
@@ -3,8 +3,18 @@ import mido
 
 class SysExConfiguration:
 
-    def __init__(self, channel_assignments: dict[int, set[int]], parameters: dict[str, int|float]) -> None:
-        pass
+    def __init__(self, channel_assignments: dict[int, set[int]], parameters: dict[int, dict[str, int|float]]) -> None:
+        '''Creates a SysExConfiguration instance
+        
+        :param dict[int, set[int]] channel assignments: The dictionnary of channel assignments. 
+            It associates to each tesla coil id the set of its assigned channels
+            {coil_id: {channel_ids}}
+        :param dict[int, dict[str, int|float]] parameters: The tesla coils' parameters
+            It associates to each tesla coil id the dictionnary of its parameters.
+            {coil_id: {parameter: value}}
+        '''
+        self.channel_assignments = channel_assignments
+        self.parameters = parameters
 
     def get_syfoh_commands(self) -> list[str]:
         pass
