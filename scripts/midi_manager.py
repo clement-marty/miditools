595ad5f46bcf3aef2179649c463f9678c1ee4c26
yedeuwu <tim.benzaquinphilippot@gmail.com>
2026-06-03 11:17:11 +0200

    Handle empty space between two spaces correctly

--- scripts/midi_manager.py
+++ scripts/midi_manager.py
@@ -10,9 +10,9 @@ class MidiManager:
         Creates a new .mid file by merging a len(file_paths) amount of midi files, by adding them one after the other while respecting the bpm changes between each other.  
         ---
         Parameter:
-        file_paths: list of strings, with the file path of each midi files
+            file_paths: list of strings, with the file path of each midi files
         Output:
-        output_path: string of the file path for the output midi file
+            output_path: string of the file path for the output midi file
         """
 
 
@@ -20,7 +20,7 @@ class MidiManager:
         time_past = 0 
         last_tempo = md.MidiFile(file_paths[0]).ticks_per_beat
         
-        #set the new file with a tbp of the first input       
+        #set the new file with the ticks per beat of the first input       
         new = md.MidiFile(ticks_per_beat=last_tempo)
         
         
@@ -36,6 +36,7 @@ class MidiManager:
 
             
             for track in temp.tracks:
+                bar_len = time_sig[0]*temp_tempo*(4/time_sig[1])
                 temp_track = md.MidiTrack()
                 temp_time = 0
                 #get the length of the current midi file
@@ -55,19 +56,22 @@ class MidiManager:
                     temp_track.append(temp_msg)
                     
                 #if last note does not end at the end of a bar, we lengthen it
-                if track_ticks > 0 & time2add<time_sig[1]*temp_tempo:
-                    print(track[i].time)
-                    print("v")
-                    print(time2add)
-                    temp_time = int(round((track[i].time+time2add)*tempo_ratio))
-                    temp_msg = track[i].copy(time=temp_time)
-                    temp_track.append(temp_msg)
+                
+                
+                #if track_ticks > 0 & time2add<time_sig[1]*temp_tempo:
+                #    print(track[i].time)
+                #    print("v")
+                #    print(time2add)
+                #    temp_time = int(round((track[i].time+time2add)*tempo_ratio))
+                #    temp_msg = track[i].copy(time=temp_time)
+                #    temp_track.append(temp_msg)
                     
                 
                 new.tracks.append(temp_track)
                 
             #add the used file length to the total time since the start of the output file           
             time_past += track_ticks
+            time_past = int(((time_past+bar_len-1)//bar_len)*bar_len) # adds empty space between 2 files if the first one does not end at a bar. 
         print(new)
         
         new.save(output_path)
@@ -75,16 +79,22 @@ class MidiManager:
 
     @classmethod
     def verify(cls, filepath: str, channel_assignments : dict[int, set[int]]) -> tuple[list[str]]:
-        pass
-    
-f1 = "miditools/midi_tests/159BPM_Beethoven.mid"
-f2 = "miditools/midi_tests/148BPM_mel.mid"
-f3 = "miditools/midi_tests/148BPM_chords.mid"
+        """
+        Verifies a few constraints to allow the Tesla coils to work properly. There should be at most 4 notes at the same time for a given coil
+        ---
+        Parameters:
+            filepath : str contaning the relative path for the midi file to verify.
+            channel_assigments : dictionnary with int key and set of int value.
+        Output:
+        """
+        mid = md.Midifile(filepath)
 
-a = MidiManager.merge([f1, f2], "miditools/midi_tests/svppp.mid")
+
+   
+
   
 # #time added = k*numerator/bpm
 
 
 # #trouver dernière time signature du fichier et ajouter a time_past le temps du fichier actuel modulo temps d'une mesure (denominator*beatpertick) - temps du fichier
-# print(4*96)s
+# print(4*96)
